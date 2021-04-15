425fe2ab4fc7eee15aa0da80b47cee560a5d7e36
yonsi <GUYANXI@mail.ustc.edu.cn>
2021-04-14 23:48:35 -0700

    draw 2D plots

--- gcc_plots.sh
+++ gcc_plots.sh
@@ -1,1 +1,2 @@
 g++ -g -std=c++17 plots.C -I$ROOTSYS/include `root-config --libs ` -lMinuit -lGenVector -o plots.exe
+g++ -g -std=c++17 2Dplots.C -I$ROOTSYS/include `root-config --libs ` -lMinuit -lGenVector -o 2Dplots.exe
